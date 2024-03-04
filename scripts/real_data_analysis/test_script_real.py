bff9e3372a0a8eebee498ed46bbc5adedc57ff65
rtandon32 <rtandon32@gatech.edu>
2024-03-04 16:15:45 -0500

    covar analysis (modifications)

--- scripts/real_data_analysis/test_script_real.py
+++ scripts/real_data_analysis/test_script_real.py
@@ -18,10 +18,17 @@ path = "/home/rtandon32/ebm/ebm_experiments/experiment_scripts/real_data/dfMri_D
 df = pd.read_csv(path)
 
 
-k=10
-# select_cols = [_ for _ in range(k) if _ not in exclude_idx]
-data = df.iloc[:,:k].values
-bm_names = df.columns.tolist()
+k=119
+exclude_idx = [21,  71,  22,  19, 111,  64,  96,  44,  90,  50,  80,  57,   9,
+        77,  33, 113,  76,  98, 105,  79,  41,   4,  87, 107, 114,  20,
+         2, 110,  84,  23,  58,   1,  60,  56,  10,  94,  49,  74,   0,
+        92,  91,  18,  61,  43,  45, 108,  78, 118,  62,   8,   5,  46,
+         3,  47,  48,  53,  42, 112,  73]
+
+select_cols = [_ for _ in range(k) if _ not in exclude_idx]
+data = df.iloc[:,select_cols]
+bm_names = data.columns.tolist()
+data = data.values
 labels = df["DX"].map({"Dementia":1, "CN":0})
 
 
@@ -33,7 +40,9 @@ elif sustainType == "mixture_KDE":
 
 L_yes                   = np.zeros(data.shape)
 L_no                    = np.zeros(data.shape)
-for i in range(k):
+
+# for i in range(k):
+for i in range(len(select_cols)):
     if sustainType == "mixture_GMM":
         L_no[:, i], L_yes[:, i] = mixtures[i].pdf(None, data[:, i])
     elif sustainType   == "mixture_KDE":
@@ -50,20 +59,20 @@ L_yes = process_L(L_yes)
 
 
 
-stage_sizes = [2,2,2,2,2]
-N_startpoints           = 50
+stage_sizes = [12,12,12,12,12]
+N_startpoints           = 40
 N_S_max                 = 1
-SuStaInLabels = df.columns[:k].tolist()
+SuStaInLabels = bm_names
 rep = 3
 N_iterations_MCMC_init = int(2e3)
 N_iterations_MCMC       = int(3e3)  #Generally recommend either 1e5 or 1e6 (the latter may be slow though) in practice
 n_stages = 5
-min_clust_size = 2
-p_absorb = 0.4
+min_clust_size = 12
+p_absorb = 0.0
 N_em = 100
 
 
-dataset_name            = 'sim_tadpole13'
+dataset_name            = 'sim_tadpole14'
 output_dir              = '/home/rtandon32/ebm/s-SuStain-outputs'
 output_folder           = os.path.join(output_dir, dataset_name + '_' + sustainType)
 if not os.path.isdir(output_folder):
